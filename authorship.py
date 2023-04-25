2766233b20dee35ec8fdf51a415612481f84f74a
erik <epautsch@lu.edu>
2023-04-24 23:11:22 -0500

    train 1

--- authorship.py
+++ authorship.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -12,6 +18,9 @@ from tensorflow.keras.callbacks import LearningRateScheduler
 from sklearn.model_selection import train_test_split
 import pandas
 import numpy as np
+
+
+# In[ ]:
 
 
 # fix random seed for reproducibility
@@ -45,26 +54,26 @@ X_train, X_test, y_train, y_test = train_test_split(sequences, labels, test_size
 top_words = 10001
 
 
-# truncate and pad input sequences
-'''
-max_review_length = 1000
-X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
-X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
-'''
+# In[ ]:
+
 
 # create the model
-embedding_vecor_length = 32
+embedding_vector_length = 128
 model = Sequential()
-model.add(Embedding(top_words, embedding_vecor_length, input_length=max_sequence_length))
+model.add(Embedding(top_words, embedding_vector_length, input_length=max_sequence_length))
 
-model.add(LSTM(100, return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
+model.add(LSTM(400, return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dropout(0.5))
-model.add(LSTM(100, return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
+model.add(LSTM(400, return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dropout(0.5))
-model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(LSTM(400, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dropout(0.5))
 
 model.add(Dense(50, activation='softmax')) # Output layer with 50 units and softmax activation
+
+
+# In[ ]:
+
 
 def cosine_annealing(epoch, initial_learning_rate, T_max):
     return initial_learning_rate * (1 + np.cos(np.pi * epoch / T_max)) / 2
@@ -75,17 +84,51 @@ T_max = 50  # Total number of epochs
 lr_scheduler = LearningRateScheduler(lambda epoch: cosine_annealing(epoch, initial_learning_rate, T_max))
 
 
+# In[ ]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cosine_annealing(epoch, initial_learning_rate, T_max):
+    return initial_learning_rate * (1 + np.cos(np.pi * epoch / T_max)) / 2
+
+initial_learning_rate = 0.001
+T_max = 50  # Total number of epochs
+
+# Generate learning rates for each epoch
+epochs = np.arange(T_max)
+learning_rates = [cosine_annealing(epoch, initial_learning_rate, T_max) for epoch in epochs]
+
+# Plot the learning rates
+plt.plot(epochs, learning_rates)
+plt.xlabel("Epoch")
+plt.ylabel("Learning Rate")
+plt.title("Cosine Annealing Learning Rate Scheduler")
+plt.grid(True)
+plt.show()
+
+
+# In[ ]:
+
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) # Use categorical_crossentropy loss for multiclass classification
 print(model.summary())
 
+
+# In[ ]:
+
+
 filepath="best_model.h5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=32, callbacks=[checkpoint])
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, batch_size=128, callbacks=[checkpoint, lr_scheduler])
+
+
+
+# In[ ]:
 
 
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
-
-# model.save('authorship_rnn_model.h5')
 
